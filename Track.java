7fb3e3f83d68048013581bd48d36a1b410a7c721
acelyakurtay <acelya.kurtay@ozu.edu.tr>
2022-12-31 23:15:29 +0300

    Update Track.java
    override//

--- Track.java
+++ Track.java
@@ -1,37 +1,57 @@
-        public int track_id;
-        public int track_duration;
-        public int track_individual_value;
-        public double[] track_sequential_value;
+import java.util.Arrays;
+
+public class Track {
+    public int trackID;
+    public int trackDuration;
+    public int track_individual_value;
+    public double[] track_sequential_value;
 
 
-        public Track(int track_id, int track_duration, int track_individual_value) {
-                this.track_id = track_id;
-                this.track_duration = track_duration;
-                this.track_individual_value = track_individual_value;
-        }
+    public Track(int trackID, int trackDuration, int track_individual_value) {
+        this.trackID = trackID;
+        this.trackDuration = trackDuration;
+        this.track_individual_value = track_individual_value;
+    }
 
-        public int getId(){
-                return track_id;
-        }
-        public void setId(int track_id){
-                this.track_id = track_id;
-        }
-        public int getDuration(){
-                return track_duration;
-        }
-        public void setDuration(int track_duration){
-                this.track_duration = track_duration;
-        }
-        public int getIndividualValue(){
-                return track_individual_value;
-        }
-        public void setIndividualValue(int track_individual_value){
-                this.track_individual_value = track_individual_value;
-        }
-        public double[] getSequentialValue(){
-                return track_sequential_value;
-        }
-        public void setSequentialValue(double[] track_sequential_value){
-                this.track_sequential_value = track_sequential_value;
-        }
+    public int getId() {
+        return trackID;
+    }
+
+    public void setId(int trackID) {
+        this.trackID = trackID;
+    }
+
+    public int getDuration() {
+        return trackDuration;
+    }
+
+    public void setDuration(int trackDuration) {
+        this.trackDuration = trackDuration;
+    }
+
+    public int getIndividualValue() {
+        return track_individual_value;
+    }
+
+    public void setIndividualValue(int track_individual_value) {
+        this.track_individual_value = track_individual_value;
+    }
+
+    public double[] getSequentialValue() {
+        return track_sequential_value;
+    }
+
+    public void setSequentialValue(double[] track_sequential_value) {
+        this.track_sequential_value = track_sequential_value;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "trackID=" + trackID +
+                ", trackDuration=" + trackDuration +
+                ", track_individual_value=" + track_individual_value +
+                ", track_sequential_value=" + Arrays.toString(track_sequential_value) +
+                '}';
+    }
 }
